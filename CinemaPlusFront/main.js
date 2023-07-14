3410bb4be84b309a24e2e14b0992bd4053ee27c3
Alexis Felipe Mauro Méndez <73500796+Alfemame@users.noreply.github.com>
2023-07-14 16:23:00 -0600

    CinemaPlus-Front
    Se suben los archivos del proyecto

---
+++ CinemaPlusFront/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 50,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
