a8768e109bac3b214aa6a95f19f32b09ab782607
Alexis Felipe Mauro Méndez <afmm160500@gmail.com>
2023-07-18 17:14:38 -0600

    Header y Main Page
    Primera parte corregida

--- CinemaPlusFront/main.js
+++ CinemaPlusFront/main.js
@@ -1,16 +1,14 @@
-var swiper = new Swiper(".home", {
-    spaceBetween: 50,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-
-  });
-  
-
-  
+// Inicializar Swiper
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
