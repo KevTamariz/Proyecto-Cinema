3283147cb5c79c4faddd97567eaeee2c6209e2e2
Alexis Felipe Mauro Méndez <afmm160500@gmail.com>
2023-07-29 13:21:41 -0600

    Nostros.js
    Pero aparece hasta abajo :c

--- CinemaPlusFront/cinema_react/src/index.js
+++ CinemaPlusFront/cinema_react/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Importa el componente raíz de la aplicación
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root') // El elemento DOM donde se renderizará la aplicación
+  document.getElementById('root')
 );
+
